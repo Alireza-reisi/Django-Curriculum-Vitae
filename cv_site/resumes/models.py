5838c945077b4a6d29b988778e5bd167420804ef
alireza reisi <alireza.reisi03127@gmail.com>
2024-04-13 02:42:16 +0430

    The resume page was made dynamic using models

--- cv_site/resumes/models.py
+++ cv_site/resumes/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Experience(models.Model):
+    title=models.CharField(max_length=50)
+    timeline=models.CharField(max_length=40)
+    description=models.TextField()
+    
+    def __str__(self):
+        return self.title
+    
+    
+class Education(models.Model):
+    title=models.CharField(max_length=50)
+    timeline=models.CharField(max_length=40)
+    description=models.TextField()
+    
+    def __str__(self):
+        return self.title
+    
+class Skill(models.Model):
+    title=models.CharField(max_length=50)
+    progress =models.IntegerField()   
+    
+    def __str__(self):
+        return self.title
--- cv_site/resumes/views.py
+++ cv_site/resumes/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
-
+from .models import Experience,Education,Skill
 def resume(request):
-    return render(request,"resume.html")
+    experiences=Experience.objects.all()
+    education=Education.objects.all()
+    skills=Skill.objects.all().order_by("progress").reverse()
+    return render(request,"resume.html",context={'experiences':experiences , 'educations':education, 'skills':skills })
